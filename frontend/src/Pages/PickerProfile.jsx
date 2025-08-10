0265d7ddd18e6cd9b135ef7f3bc56bc3b5097c24
subratamondal2 <subrata24012005@gmail.com>
2025-08-10 10:30:28 +0530

    feat: add EditPickerProfile component for updating picker information

---
+++ frontend/src/Pages/PickerProfile.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PickerProfile from '../components/core/pickerProfile/PickerProfile';
+
+const PickerProfilePage = () => {
+  return <PickerProfile />;
+};
+
+export default PickerProfilePage;
